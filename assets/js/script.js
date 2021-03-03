694dbf0b3dfef01c46b3820c1742c82799a56fee
The Podz <stefanpodz@yahoo.com>
2021-03-03 18:29:42 -0500

    updated html and js display code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 var searchBtn = document.getElementById('searchBtn');
+var cityH = document.getElementById('city');
+var tempP = document.getElementById('temp');
+var humP = document.getElementById('hum');
+var windP = document.getElementById('wind');
+var uvP = document.getElementById('uv');
 
+// This function allows the user to enter a city of their choice and click the search button to retrieve forecast APIs
 function getSearchVal(event) {
     event.preventDefault();
     var citySearch = document.getElementById('city-search').value;
@@ -8,50 +14,70 @@ function getSearchVal(event) {
     console.log("getSearchVal is working!");
 }
 
-
+// This function returns a current forecast API
 function oneDayCall(city) {
     console.log("oneDayCall is working!");
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=032af0ff95709be09e01f89ce7ab1a46', {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=032af0ff95709be09e01f89ce7ab1a46&units=imperial', {
         headers: {
             'Accept': 'application/json'
         }
     })
-    
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    }).then(function (data)  {
-        console.log('RESPONSE ', data)
-        console.log('RESPONSE ', data.coord)
-        fiveDayCall(city);
-        getUVIndex(data.coord.lat, data.coord.lon);
-    })
-    .catch(function (err)  {
-        console.error(err);
-    });
-    
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        }).then(function (data) {
+            console.log('City -----', data)
+            fiveDayCall(city);
+            getUVIndex(data.coord.lat, data.coord.lon);
+
+            tempP.textContent = `Temprature: ${data.main.temp}`
+            humP.textContent = `Humidity: ${data.main.humidity}`
+
+
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+
 }
 
+// localstorage
+var localArr = JSON.parse(localStorage.getItem("cities")) || [];
+
 function fiveDayCall(city) {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=032af0ff95709be09e01f89ce7ab1a46', {
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=032af0ff95709be09e01f89ce7ab1a46', {
         headers: {
             'Accept': 'application/json'
         }
     })
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    }).then(function (data)  {
-        console.log('RESPONSE ', data)
-    })
-    .catch(function (err)  {
-        console.error(err);
-    });
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        }).then(function (data) {
+            console.log('RESPONSE ', data)
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
     console.log("five day forecast");
 }
+
 function getUVIndex(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=032af0ff95709be09e01f89ce7ab1a46`, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        }).then(function (data) {
+            console.log('RESPONSE ------------------------ ', data)
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
     console.log("uv index");
 }
 
 document.getElementById('searchBtn').addEventListener('click', getSearchVal);
-// searchBtn.addEventListener
